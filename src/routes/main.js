cc140784b33fe8b71c5254b39e7c96c7bfbd8d1e
Kenneth <kijbutad08@gmail.com>
2025-10-24 23:03:47 +0800

    Update sections

--- src/routes/main.js
+++ src/routes/main.js
@@ -3,14 +3,14 @@ const path = require('path');
 const router = express.Router();
 
 const projects = require('../data/projects.json');
-const testimonials = require('../data/testimonials.json');
+// const testimonials = require('../data/testimonials.json');
 const blog = require('../data/blog.json');
 
 router.get('/', (req, res) => {
   res.render('index', { 
     title: 'Kenneth Irvin Butad | Professional Virtual Assistant',
     projects: projects,
-    testimonials: testimonials,
+    // testimonials: testimonials,
     blog: blog,
     currentYear: new Date().getFullYear()
   });
